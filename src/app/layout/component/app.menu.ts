de1d38d198efa3c4ddcc107618f663aa04b2f3d6
Sebastian Cerviño <cervisebas@gmail.com>
2025-08-21 20:55:23 -0300

    first commit

---
+++ src/app/layout/component/app.menu.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { AppMenuitem } from './app.menuitem';
+
+@Component({
+    selector: 'app-menu',
+    standalone: true,
+    imports: [CommonModule, AppMenuitem, RouterModule],
+    template: `<ul class="layout-menu">
+        <ng-container *ngFor="let item of model; let i = index">
+            <li app-menuitem *ngIf="!item.separator" [item]="item" [index]="i" [root]="true"></li>
+            <li *ngIf="item.separator" class="menu-separator"></li>
+        </ng-container>
+    </ul> `
+})
+export class AppMenu {
+    model: MenuItem[] = [];
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Menu',
+                items: [
+                    {
+                        label: 'Presupuestar',
+                        icon: 'pi pi-file-edit',
+                        routerLink: ['/uikit/formlayout']
+                    },
+                    {
+                        label: 'Trabajos',
+                        icon: 'pi pi-hammer',
+                        routerLink: ['/uikit/formlayout']
+                    },
+                    {
+                        label: 'Clientes',
+                        icon: 'pi pi-users',
+                        routerLink: ['/uikit/formlayout']
+                    },
+                    {
+                        label: 'Materiales',
+                        icon: 'pi pi-box',
+                        routerLink: ['/uikit/formlayout']
+                    },
+                    {
+                        label: 'Proveedores',
+                        icon: 'pi pi-building',
+                        routerLink: ['/uikit/formlayout']
+                    },
+                ],
+            },
+        ];
+    }
+}
---
+++ src/app/layout/component/app.topbar.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { StyleClassModule } from 'primeng/styleclass';
+import { LayoutService } from '../service/layout.service';
+
+@Component({
+    selector: 'app-topbar',
+    standalone: true,
+    imports: [RouterModule, CommonModule, StyleClassModule],
+    template: ` <div class="layout-topbar">
+        <div class="layout-topbar-logo-container">
+            <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
+                <i class="pi pi-bars"></i>
+            </button>
+            <a class="layout-topbar-logo" routerLink="/">
+                <img
+                    src="assets/presupuestito-icono.png"
+                    class="h-[3rem]"
+                    alt="Icono"
+                >
+                <span class="text-yellow-950">
+                    PRESUPUESTITO
+                </span>
+            </a>
+        </div>
+    </div>`
+})
+export class AppTopbar {
+    items!: MenuItem[];
+
+    constructor(public layoutService: LayoutService) {}
+
+}
