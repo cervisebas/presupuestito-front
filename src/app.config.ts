c61c505e813ab6f638f7bd3b00baa88583f45415
“Sebástian <cervisebas@gmail.com>
2025-08-28 19:46:28 -0300

    Agregar servicios y constantes para el manejo de presupuestos, categorías, clientes, costos, empleados, facturas, materiales, pagos y salarios; además, configurar entornos de desarrollo y producción.

--- src/app.config.ts
+++ src/app.config.ts
@@ -11,6 +11,7 @@ export const appConfig: ApplicationConfig = {
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch()),
         provideAnimationsAsync(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+        provideHttpClient(),
     ]
 };
---
+++ src/app/common/api/services/budget.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Endpoints } from '../constants/Endpoints';
+import { BudgetResponse } from '../interfaces/responses/BudgetResponse';
+import { BudgetRequest } from '../interfaces/requests/BudgetRequest';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Budget {
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  public getBudgets() {
+    return this.http.get<BudgetResponse[]>(
+      Endpoints.BUDGET,
+    );
+  }
+
+  public getBudgetById(id_budget: string) {
+    return this.http.get<BudgetResponse>(
+      `${Endpoints.BUDGET}/${id_budget}`,
+    );
+  }
+
+  public getBudgetByClientId(id_client: string) {
+    return this.http.get<BudgetResponse[]>(
+      `${Endpoints.BUDGETS_BY_CLIENT}/${id_client}`,
+    );
+  }
+
+  public getBudgetCost(id_budget: string) {
+    return this.http.get<number>(
+      `${Endpoints.BUDGET_PRICE}/${id_budget}`,
+    );
+  }
+
+  public createBudget(data: BudgetRequest) {
+    return this.http.post<void>(
+      Endpoints.BUDGET,
+      data,
+    );
+  }
+  
+  public updateBudget(data: BudgetRequest) {
+    return this.http.put<void>(
+      Endpoints.BUDGET,
+      data,
+    );
+  }
+  
+  public deleteBudget(id_budget: number) {
+    return this.http.delete<void>(
+      `${Endpoints.BUDGET}/${id_budget}`,
+    );
+  }
+  
+
+}
